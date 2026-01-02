3712392874eeaab1d1ac2d5e483c2f322401edce
Harshit_Parmar <parmarharshit441@gmail.com>
2026-01-02 11:29:34 +0530

    setup mongoDB connection function with mongoose !

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { morganStream } from "./utils/logger.js";
+
+const app = express();
+
+// middlewares
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan("combined", {
+    stream: morganStream,
+  })
+);
+
+export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+// dotenv config
+import dotenv, { config } from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/database.js";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error : ", error);
+  });
