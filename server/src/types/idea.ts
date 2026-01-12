0357d846cae1267ca32bfc78cca0d3e7e5364952
Harshit_Parmar <parmarharshit441@gmail.com>
2026-01-12 22:58:44 +0530

    changes in idea model !

---
+++ server/src/types/idea.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+interface ITechnology {
+  name: string;
+}
+
+interface IIdea {
+  title: string;
+  description: string;
+  owner: mongoose.Types.ObjectId;
+  technologies: ITechnology[];
+  status: "draft" | "open" | "in-progress" | "completed" | "archived";
+  slug?: string;
+  lookingForCollaboratos: boolean;
+  requirements: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type { ITechnology, IIdea };
