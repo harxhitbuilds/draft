9764e819b276d0e022fac1de5f0a462ee92ce614
Harshit_Parmar <parmarharshit441@gmail.com>
2026-02-21 23:59:17 +0530

    second deployment error resolved !

--- client/components/global/components/markdown.tsx
+++ client/components/global/components/markdown.tsx
@@ -1,12 +1,15 @@
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
+
+import ReactMarkdown from "react-markdown";
 
 interface MarkdownProps {
-  content: string;
+  content?: string | null;
 }
 
 export default function Markdown({ content }: MarkdownProps) {
+  if (!content) return null;
+
   return (
     <article className="prose prose-neutral dark:prose-invert max-w-none">
       <ReactMarkdown
